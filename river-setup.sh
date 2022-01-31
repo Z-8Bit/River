a6bc2a4d2deb81d5278ce67a1a87e665d677e1fe
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2022-01-31 09:32:23 +0530

    script fixes

--- river-setup.sh
+++ river-setup.sh
@@ -1,10 +1,8 @@
 sudo pacman -Syu --needed base-devel
 git clone https://aur.archlinux.org/paru-bin.git
-cd paru
-makepkg -si
+cd paru-bin && makepkg -si
 
-paru -S --noconfirm alacritty backlight_control bluez bluez-utils blueman btop cmatrix discord dunst firefox-developer-edition fluent-cursor-theme-git github-desktop-bin grim gvfs htop libappindicator-gtk2 lollypop lxappearance maim moc mpv
-multimc-bin neofetch nerd-fonts-jetbrains-mono network-manager-applet nitrogen noto-fonts-emoji ntfs-3g pamixer pavucontrol polkit-gnome pcmanfm pipewire pipewire-alsa pipewire-pulse python-dbus powertop qbittorrent-dark-git qogir-gtk-theme-git river rofi-lbonn-wayland-git spotify spotify-adblock slurp teams-for-linux tela-icon-theme-bin thunar ttf-unifont ttf-ms-fonts viewnior vscodium-bin waybar-git wireplumber wl-clipboard whatsapp-nativefier-dark zoom
+paru -S --noconfirm alacritty alsa-tools alsa-utils backlight_control bluez bluez-utils blueman btop dbus-python discord dunst firefox-developer-edition fluent-cursor-theme-git github-desktop-bin grim gparted gvfs htop jdk-temurin light libappindicator-gtk2 lollypop lxappearance maim microsoft-edge-stable-bin moc mpv multimc-bin neofetch nerd-fonts-jetbrains-mono network-manager-applet nitrogen noto-fonts-emoji ntfs-3g pamixer pavucontrol polkit-gnome pcmanfm pipewire pipewire-alsa pipewire-pulse python-gobject powertop qbittorrent-dark-git qogir-gtk-theme-git river rofi-lbonn-wayland-git spotify spotify-adblock slurp teams-for-linux tela-icon-theme thunar transmission-gtk ttf-ms-fonts ttf-unifont viewnior vscodium-bin waybar-git wireplumber wl-clipboard whatsapp-nativefier-dark zoom
 
 for service in bluetooth NetworkManager; do
   sudo systemctl enable --now $service
@@ -12,10 +10,11 @@ done
 
 git clone https://github.com/Z-8Bit/River.git ~/Downloads/River
 
-mkdir -p ~/.config/{alacritty,dunst,neofetch,pcmanfm,river,rofi,waybar}
+mkdir -p ~/.config/{alacritty,dunst,moc,neofetch,pcmanfm,river,rofi,waybar}
 
 cp ~/Downloads/River/alacritty/alacritty.yml ~/.config/alacritty/
 cp -r ~/Downloads/River/dunst/* ~/.config/dunst/
+cp ~/Downloads/River/moc/* ~/.config/moc/
 cp ~/Downloads/River/neofetch/config.conf ~/.config/neofetch/
 cp -r ~/Downloads/River/pcmanfm/* ~/.config/pcmanfm/
 sudo chmod +x ~/Downloads/River/river/* && cp ~/Downloads/River/river/* ~/.config/river/
@@ -29,12 +28,12 @@ sudo cp ~/Downloads/River/scripts/*.sh /usr/bin/
 git clone https://github.com/Z-8Bit/Wallpapers ~/Pictures/Wallpapers
 RED='\033[0;31m'
 
-# sudo pacman -S refind && refind-install
+# refind-install
 # git clone https://github.com/josephsurin/refind-theme-circle.git && sudo rm -r ./refind-theme-circle/{screenshots,.git}
 # sudo cp -r refind-theme-circle /boot/efi/EFI/refind/ && sudo echo "include refind-theme-circle/theme.conf" >> /boot/efi/EFI/refind/refind.conf
 
 sleep 5
-echo -e "${RED}PLEASE REBOOT NOW"
+echo -e "${RED}Please Reboot Now."
 
 
 
